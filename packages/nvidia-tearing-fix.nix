0ea42545c3644b3161408240f091f0321827d78c
Andrei Vladescu-Olt <andrei@avolt.net>
2018-11-02 21:31:53 +0100

    update

--- packages/nvidia-tearing-fix.nix
+++
@@ -1,8 +0,0 @@
-self: super: with super; {
-
-nvidia-tearing-fix = writeShellScriptBin "nvidia-tearing-fix" ''
-  nvidia-settings --assign CurrentMetaMode='\
-    DP-0: nvidia-auto-select +0+0 {ForceCompositionPipeline=On}, \
-    DP-2: nvidia-auto-select +0+0 {ForceCompositionPipeline=On, SameAs=DP-0}' '';
-
-}
