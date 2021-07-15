2bd9ae98735d58c2ab004f8ee12b5f30d9250696
Andrei Volt <andrei@avolt.net>
2021-07-15 11:56:59 +0200

    update

--- packages/zprint.nix
+++ packages/zprint.nix
@@ -32,10 +32,29 @@ stdenv.mkDerivation rec {
       $out/bin/zprint
   '';
 
-  meta = with stdenv.lib; {
-    homepage = https://github.com/kkinnear/zprint;
-    description = "Library to reformat Clojure and Clojurescript source code and s-expressions ";
-    license = licenses.free;
-    platforms = platforms.linux;
-  };
+  # meta = with stdenv.lib; {
+  #   homepage = https://github.com/kkinnear/zprint;
+  #   description = "Library to reformat Clojure and Clojurescript source code and s-expressions ";
+  #   license = licenses.free;
+  #   platforms = platforms.linux;
+  # };
 }
+
+# zprint = pkgs.stdenv.mkDerivation rec {
+#   name = "zprint";
+#   src = pkgs.fetchurl {
+#     url = "https://github.com/kkinnear/zprint/releases/download/0.4.10/zprintl-0.4.10";
+#     sha256 = "0iab2gvynb0njhr2vy26li165sc2v6p5pld7ifp6jlf7yj3yr4gl";
+#   };
+#   unpackPhase = ":";
+#   dontStrip = true;
+#   preFixup =
+#     let rpath = with pkgs; lib.makeLibraryPath [ zlib ];
+#     in ''
+#       patchelf \
+#         --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+#         --set-rpath "${rpath}" \
+#         $out/bin/zprint
+#     '';
+#   installPhase = "mkdir -p $out/bin && cp $src $out/bin/zprint && chmod +x $out/bin/zprint";
+# };
