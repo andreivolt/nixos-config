7406048f7b3bf9db8d45515831d03fb1c888526d
Andrei Volt <andrei@avolt.net>
2021-07-15 14:08:55 +0200

    update

--- modules.d/kdeconnect.nix
+++ modules.d/kdeconnect.nix
@@ -1,4 +1,10 @@
+{ config, lib, ... }:
+
 {
+  home-manager.users.avo = { pkgs, config, ... }: {
+    services.kdeconnect.enable = true;
+  };
+
   networking.firewall.allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
   networking.firewall.allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
 }
