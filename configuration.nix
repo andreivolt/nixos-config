88477827667f0e9ed7fb3a40ce7f64c6ce8784d4
Andrei Volt <andrei@avolt.net>
2021-07-15 14:10:20 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -241,57 +241,14 @@ in {
         rev = "8c1b2b85ba40b9afecc87990c884fe5cf9ac56d1"; sha256 = "0aa87r82w431445n4n6brfyzh3bnrcf5s3lhih1493yc5mzjnjh3";
       }}/autopair.zsh
 
-      bindkey -v
+      source ${./modules.d/zsh/zsh.d/vi.zsh}
 
-      export KEYTIMEOUT=1
-
-      autoload -U edit-command-line; zle -N edit-command-line
-      autoload -U select-bracketed; zle -N select-bracketed
-      autoload -U select-quoted; zle -N select-quoted
-      autoload -U surround; zle -N delete-surround surround; zle -N change-surround surround; zle -N add-surround surround
-
-      bindkey -M vicmd '^x^e' edit-command-line; bindkey -M viins '^x^e' edit-command-line
-      bindkey -M vicmd 'H' run-help
-
-      bindkey -M vicmd v edit-command-line
-
-      bindkey ''${terminfo[kcbt]:-^\[\[Z} reverse-menu-complete
-
-      bindkey '^n' expand-or-complete
-      bindkey '^p' reverse-menu-complete
-
-      for m in visual viopp; do
-        for c in {a,i}''${(s..)^:-'()[]{}<>bB'}; do
-          bindkey -M $m $c select-bracketed
-        done
-      done
-      for m in visual viopp; do
-        for c in {a,i}{\',\",\`}; do
-          bindkey -M $m $c select-quoted
-        done
-      done
-
-      bindkey -a cs change-surround
-      bindkey -a ds delete-surround
-      bindkey -a ys add-surround
-      bindkey -M visual S add-surround
-
-      function zle-keymap-select zle-line-init zle-line-finish {
-        case $KEYMAP in
-          vicmd) print -n '\033[1 q' ;;
-          viins|main) print -n '\033[6 q' ;;
-        esac
-      }
-      zle -N zle-line-init; zle -N zle-line-finish; zle -N zle-keymap-select
-
+      source ${pkgs.fzf}/share/fzf/completion.zsh
       zstyle ':completion:*' menu select
       zstyle ':completion:*' rehash true
 
-      source ${pkgs.fzf}/share/fzf/completion.zsh
-
       HISTSIZE=99999 SAVEHIST=$HISTSIZE
       HISTFILE=~/.cache/zsh_history
-
       setopt \
         extended_history \
         hist_ignore_all_dups \
