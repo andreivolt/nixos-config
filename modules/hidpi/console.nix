38a6adb12351e651dc4751e472e9ca06de4495b4
Andrei Volt <andrei@avolt.net>
2021-08-15 14:25:20 +0200

    update

--- modules/hidpi/console.nix
+++ modules/hidpi/console.nix
@@ -1,7 +1,13 @@
+{ pkgs, ... }:
+
 {
-  console.font = "latarcyrheb-sun32";
+  console.packages = [ pkgs.terminus_font ];
+
+  console.font = "ter-132n";
+
   # user font in initrd
   console.earlySetup = true;
+
   # # use maximum resolution in systemd-boot
   # boot.loader.systemd-boot.consoleMode = lib.mkDefault "max";
 }
