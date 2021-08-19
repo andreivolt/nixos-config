ab727593f8f9091e7edfb2cd9a7effa05877bb4e
Andrei Volt <andrei@avolt.net>
2021-08-19 02:38:51 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -213,7 +213,7 @@
       (hunspellWithDicts (with hunspellDicts; [ en-us fr-moderne ])) # TODO
       (lowPrio mandoc)
       (zathura.override { useMupdf = true; })
-      abduco
+      abduco # terminal
       acpi
       adb-sync
       alsaPlugins
@@ -642,9 +642,27 @@
           letter-spacing = "-0.5";
         };
 
-        colors = {
-          foreground = "ffffff";
-          background = "000000";
+        colors = with (import ./modules/theme.nix); {
+          foreground = foreground;
+          background = background;
+
+          regular0 = black_bg;
+          regular1 = red_fg;
+          regular2 = green_fg;
+          regular3 = yellow_fg;
+          regular4 = blue_fg;
+          regular5 = magenta_fg;
+          regular6 = cyan_fg;
+          regular7 = white_fg;
+
+          bright0 = black_bg;
+          bright1 = red_bg;
+          bright2 = green_bg;
+          bright3 = yellow_bg;
+          bright4 = blue_bg;
+          bright5 = magenta_bg;
+          bright6 = cyan_bg;
+          bright7 = white_bg;
         };
 
         mouse = {
