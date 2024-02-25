5a98b7bd64b1a3580570026cca1cc1ddb1ec2ed0
Andrei Volt <andrei@avolt.net>
2024-02-25 12:34:46 +0100

    update

--- modules/mac_screenshots.nix
+++ modules/mac_screenshots.nix
@@ -1,6 +1,7 @@
 {
-  # screenshot to clipboard default
+  # screenshot to clipboard by default
   system.defaults.screencapture.location = "Clipboard";
+
   # window screenshot disable shadows
   system.defaults.screencapture.disable-shadow = true;
 }
