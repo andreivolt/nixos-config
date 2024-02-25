cc8cda8273281856b5f5bb88a92ce70e3a3db357
Andrei Volt <andrei@avolt.net>
2024-02-25 12:22:33 +0100

    update

--- modules/chrome/default.nix
+++ modules/chrome/default.nix
@@ -2,27 +2,17 @@
   security.chromiumSuidSandbox.enable = true;
 
   programs.chromium = {
-      # homepageLocation = "https://www.google.com";
-      # defaultSearchProviderSuggestURL = "https://encrypted.google.com/complete/search?output=chrome&q={searchTerms}";
-      # defaultSearchProviderSearchURL = "https://encrypted.google.com/search?q={searchTerms}&{google:RLZ}{google:originalQueryForSuggestion}{google:assistedQueryStats}{google:searchFieldtrialParameter}{google:searchClient}{google:sourceId}{google:instantExtendedEnabledParameter}ie={inputEncoding}";
-      enable = true;
-      extensions = import ./chrome-extensions.nix;
-      extraOpts = {
-        # "BrowserSignin" = 0;
-        "WelcomePageOnOSUpgradeEnabled" = false;
-        # "SyncDisabled" = true;
-        # "PasswordManagerEnabled" = false;
-        "DefaultBrowserSettingEnabled" = false;
-        "SpellcheckEnabled" = true;
-        "SpellcheckLanguage" = [
-          "fr-FR"
-          "en-US"
-          "ro"
-        ];
-        # "JavascriptEnabled" = false;
-        # "ManagedBookmarks" = [
-        #   { name = "example.com"; url = "https://example.com"; }
-        # ];
-      };
+    enable = true;
+    extensions = import ./chrome-extensions.nix;
+    extraOpts = {
+      "WelcomePageOnOSUpgradeEnabled" = false;
+      "DefaultBrowserSettingEnabled" = false;
+      "SpellcheckEnabled" = true;
+      "SpellcheckLanguage" = [
+        "fr-FR"
+        "en-US"
+        "ro"
+      ];
     };
- }
+  };
+}
