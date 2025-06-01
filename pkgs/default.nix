a070855eb3212a574667006f3be8dc10bd4506f7
Andrei Volt <andrei@avolt.net>
2025-06-02 00:06:48 +0200

    update

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,7 @@ with builtins; let
   currentDirFiles = attrNames (readDir ./.);
   isNixFile = name: name != "default.nix" && (hasSuffix ".nix" name || pathExists (./. + "/${name}/default.nix"));
   nixFiles = filter isNixFile currentDirFiles;
-  
+
   # Create a lazy attribute set - packages are only evaluated when accessed
   packages = listToAttrs (map (name: {
     name = name;  # Use directory name as-is
