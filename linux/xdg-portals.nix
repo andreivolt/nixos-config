b689b570867149f3259eaff9726edc9b7d3d8bbd
Andrei Volt <andrei@avolt.net>
2025-06-01 19:36:55 +0200

    fix remote builder deprecations

--- linux/xdg-portals.nix
+++ linux/xdg-portals.nix
@@ -2,6 +2,7 @@
   xdg.portal = {
     enable = true;
     wlr.enable = true;
+    config.common.default = "*";
     extraPortals = with pkgs; [
       xdg-desktop-portal-wlr
       xdg-desktop-portal-gtk
