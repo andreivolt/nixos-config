7c1fbdb50f201daf7a2f5c5df8204cecdc290fd0
Andrei Volt <andrei@avolt.net>
2025-06-01 18:18:42 +0200

    update

--- darwin/apply-settings.nix
+++
@@ -1,6 +0,0 @@
-{lib, ...}: {
-  # system.activationScripts.applySettings.text = lib.mkAfter ''
-  #   echo 'apply settings immediately'
-  #   /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u 2>/dev/null || true
-  # '';
-}
