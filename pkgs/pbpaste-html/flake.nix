0f073e8a7a3a3fa40f22cb62cd6aa3eba982bb52
Andrei Volt <andrei@avolt.net>
2025-06-01 19:11:46 +0200

    update

---
+++ pkgs/pbpaste-html/flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "Tool to print HTML content from the clipboard using Swift and Cocoa";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.callPackage ./default.nix {};
+    packages.x86_64-darwin.default = nixpkgs.legacyPackages.x86_64-darwin.callPackage ./default.nix {};
+  };
+}
