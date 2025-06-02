83084132f37fb2d8d0ada9efb33ed1ee1d983925
Andrei Volt <andrei@avolt.net>
2025-06-02 03:36:49 +0200

    update

--- shared/tor.nix
+++ shared/tor.nix
@@ -1,10 +1,17 @@
-{
-  services.tor = {
+{lib, pkgs, ...}: {
+  # Linux: Use services.tor
+  services.tor = lib.mkIf pkgs.stdenv.isLinux {
     enable = true;
-
     client.enable = true;
     client.dns.enable = true;
-
     torsocks.enable = true;
   };
+
+  # macOS: Use homebrew
+  homebrew.brews = lib.mkIf pkgs.stdenv.isDarwin [
+    {
+      name = "tor";
+      restart_service = true;
+    }
+  ];
 }
