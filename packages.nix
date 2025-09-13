b4136779ab23f8f5766980eb20f3e4a169f9b396
Andrei Volt <andrei@avolt.net>
2025-09-13 09:02:23 +0200

    packages

--- packages.nix
+++ packages.nix
@@ -1,6 +1,9 @@
 pkgs: with pkgs; [
   # ghostty # TODO
+  age
   alejandra # nix formatter
+  spotify
+  ansifilter
   android-tools
   aria2
   arp-scan
@@ -33,8 +36,6 @@ pkgs: with pkgs; [
   dwdiff
   edir
   erdtree
-  tree-sitter
-  pry
   eslint
   eza
   fastgron
@@ -59,9 +60,6 @@ pkgs: with pkgs; [
   gojq
   google-chrome
   google-cloud-sdk
-  timg
-  procs
-  jujutsu
   gum
   htmlq
   htop-vim
@@ -72,6 +70,7 @@ pkgs: with pkgs; [
   jet
   jo
   json2nix
+  jujutsu
   just
   lastpass-cli
   lazydocker
@@ -106,6 +105,8 @@ pkgs: with pkgs; [
   pnpm
   poppler-utils
   portaudio
+  procs
+  pry
   pstree
   pulumi-bin
   pv
@@ -120,14 +121,13 @@ pkgs: with pkgs; [
   ruff
   rust-script
   rustc
-  cargo
   rustfmt
+  # rustup
   scc
   scrcpy
   shell-gpt
   socat
   sops
-  age
   sox
   sqlite
   sqlite-utils
@@ -138,8 +138,10 @@ pkgs: with pkgs; [
   telegram-desktop
   terminal-colors
   tidyp
+  timg
   tmux
   tree
+  tree-sitter
   trurl
   upterm
   uv
