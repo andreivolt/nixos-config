207ec30f9bae7bd6fd9de3e1978f0d0255e0369c
Andrei Volt <andrei@avolt.net>
2025-10-22 14:38:58 +0200

    update

--- darwin/google-drive.nix
+++ darwin/google-drive.nix
@@ -6,9 +6,4 @@
     PromptToBackupDevices = false;
     SearchHotKey = 0;
   };
-
-  system.activationScripts.googleDriveSymlinks.text = ''
-    echo 'create Google Drive symlinks'
-    sudo -u andrei ln -sfn "/Users/andrei/Google Drive/My Drive" /Users/andrei/drive
-  '';
 }
