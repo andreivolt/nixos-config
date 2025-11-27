1facb792ac7817a5b67f3b056081728892a271a2
Andrei Volt <andrei@avolt.net>
2025-11-27 17:45:04 +0100

    hyprland: use systemd services for auto-pin script

--- linux/hyprland/scripts/auto-pin.sh
+++ linux/hyprland/scripts/auto-pin.sh
@@ -6,7 +6,7 @@
 
 pattern="$1"
 
-socat - UNIX-CONNECT:"$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do
+socat -t 999999 - UNIX-CONNECT:"$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do
     case "$line" in
         openwindow*)
             addr="${line#*>>}"
