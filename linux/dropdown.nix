50ab9f526426e5e630cbe51e763bf5679b7fe605
Andrei Volt <andrei@avolt.net>
2025-11-28 20:46:45 +0100

    fix: use login shell for dropdown terminal

--- linux/dropdown.nix
+++ linux/dropdown.nix
@@ -3,7 +3,8 @@
 let
   terminal = pkgs.kitty;
   # --single-instance keeps one kitty process, --instance-group dropdown ensures dropdown windows share same instance
-  terminalCommand = "${terminal}/bin/kitty --single-instance --instance-group dropdown --class dropdown";
+  # -1 runs kitty in single-instance mode with login shell
+  terminalCommand = "${terminal}/bin/kitty --single-instance --instance-group dropdown --class dropdown ${pkgs.zsh}/bin/zsh --login";
 in {
   # Dropdown terminal service
   home-manager.users.andrei = { config, pkgs, ... }: {
