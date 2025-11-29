22e805cbfbd0164b10d47334985634ed387e40e6
Andrei Volt <andrei@avolt.net>
2025-11-29 15:46:55 +0100

    update GTK theme config with Breeze and Tela icons

--- linux/gtk.nix
+++ linux/gtk.nix
@@ -1,6 +1,24 @@
-{
-  home-manager.users.andrei.gtk = {
-    enable = true;
-    theme.name = "Breeze-Dark";
+{pkgs, ...}: {
+  home-manager.users.andrei = {
+    home.packages = with pkgs; [
+      tela-icon-theme
+      noto-fonts
+    ];
+
+    gtk = {
+      enable = true;
+      theme.name = "Breeze";
+      iconTheme.name = "Tela-dark";
+      font = {
+        name = "Noto Sans";
+        size = 10;
+      };
+      gtk3.extraConfig = {
+        gtk-application-prefer-dark-theme = true;
+      };
+      gtk4.extraConfig = {
+        gtk-application-prefer-dark-theme = true;
+      };
+    };
   };
 }
