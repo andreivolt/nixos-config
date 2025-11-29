182f24d85acf04c8d49d3333ab6bddf942c23112
Andrei Volt <andrei@avolt.net>
2025-11-29 15:47:08 +0100

    restructure packages into base and extra sets

---
+++ linux/home.nix
@@ -0,0 +1,55 @@
+# Shared home-manager config for linux systems
+# extraPackagesFile: optional file with additional packages (e.g. linux/packages-extra.nix)
+{ config, inputs, extraPackagesFile ? null }:
+
+{pkgs, ...}: {
+  imports = [
+    ../shared/rust-script-warmer.nix
+    inputs.vicinae.homeManagerModules.default
+  ];
+
+  home.stateVersion = "24.05";
+  home.enableNixpkgsReleaseCheck = false;
+  nixpkgs.config = config.nixpkgs.config;
+  nixpkgs.overlays = config.nixpkgs.overlays;
+
+  home.packages =
+    (import "${inputs.self}/packages.nix" pkgs)
+    ++ (if extraPackagesFile != null then import extraPackagesFile pkgs else []);
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = false;
+    initContent = "source ~/.config/zsh/rc.zsh";
+  };
+
+  services.playerctld.enable = true;
+  services.wob.enable = true;
+  services.vicinae = {
+    enable = true;
+    autoStart = true;
+  };
+
+  xdg.enable = true;
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    documents = "~/documents";
+    download = "~/downloads";
+  };
+  xdg.mimeApps.enable = true;
+  xdg.mimeApps.defaultApplications = let
+    browser = "firefox";
+  in {
+    "application/pdf" = "org.pwmt.zathura.desktop";
+    "image/jpeg" = "imv.desktop";
+    "image/png" = "imv.desktop";
+    "inode/directory" = "thunar.desktop";
+    "text/html" = "${browser}.desktop";
+    "text/plain" = "sublime_text.desktop";
+    "video/mp4" = "mpv.desktop";
+    "x-scheme-handler/http" = "${browser}.desktop";
+    "x-scheme-handler/https" = "${browser}.desktop";
+  };
+  xdg.configFile."mimeapps.list".force = true;
+}
