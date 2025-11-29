d382ebbb5e3ae7a07e9d0ce378683561a562d094
Andrei Volt <andrei@avolt.net>
2025-11-29 16:15:50 +0100

    use services.udev.extraHwdb for fkey remap

--- linux/fkey-remap.nix
+++ linux/fkey-remap.nix
@@ -2,16 +2,9 @@
 
 # Remap F1-F4 to number keys 1-4 at kernel level (hwdb)
 # This works in console and Wayland/X11
-# Virtual consoles are moved to Ctrl+Alt+F5-F12 instead of F1-F4
 {
-  # Create hwdb file directly in /etc/udev/hwdb.d/
-  # services.udev.extraHwdb doesn't seem to work reliably, so we use environment.etc
-  environment.etc."udev/hwdb.d/99-fkey-remap.hwdb".text = ''
-    # Remap F1-F4 to number keys 1-4 (AZERTY keyboard)
-    # Works at kernel level, affects console and all graphical sessions
-    # On AZERTY, these keycodes produce: 1=&, 2=é, 3=", 4='
-    # With Shift: 1, 2, 3, 4
-    # Scancodes verified for ThinkPad AT keyboard
+  # Use services.udev.extraHwdb which automatically compiles the hwdb
+  services.udev.extraHwdb = ''
     evdev:atkbd:*
      KEYBOARD_KEY_3b=key_1
      KEYBOARD_KEY_3c=key_2
