1e411dd17a853b35b31bb535611b464c832913af
Andrei Volt <andrei@avolt.net>
2025-11-29 21:50:41 +0100

    Add nix-community binary cache for pre-built packages

--- shared/nix.nix
+++ shared/nix.nix
@@ -4,6 +4,17 @@
     options = "--delete-older-than 30d";
   };
 
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
