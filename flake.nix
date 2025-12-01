aaab1d956537cf937f142b861debab839d139c3f
Andrei Volt <andrei@avolt.net>
2025-12-01 23:02:57 +0100

    add sublime4 and swayimg

--- flake.nix
+++ flake.nix
@@ -133,6 +133,9 @@
         {
           nixpkgs.hostPlatform = "aarch64-linux";
           nixpkgs.config.allowUnfree = true;
+          nixpkgs.config.permittedInsecurePackages = [
+            "openssl-1.1.1w"
+          ];
           nixpkgs.overlays = [
             nixos-apple-silicon.overlays.default
           ] ++ commonNixpkgsConfig.overlays;
