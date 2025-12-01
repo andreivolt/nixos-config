3f4693e98504d073c066b0e3b009daaf76c40c24
Andrei Volt <andrei@avolt.net>
2025-12-01 21:44:54 +0100

    add NIXOS_OZONE_WL and gsettings schema

--- linux/gtk.nix
+++ linux/gtk.nix
@@ -1,8 +1,25 @@
 {pkgs, ...}: {
+  # Ensure gsettings schemas are available system-wide
+  environment.systemPackages = with pkgs; [
+    gsettings-desktop-schemas
+    glib  # For gsettings command
+  ];
+
+  # Required for gsettings to find schemas
+  programs.dconf.enable = true;
+
+  # Set GSETTINGS_SCHEMA_DIR so gsettings can find schemas
+  # NIXOS_OZONE_WL enables native Wayland for Electron/Chromium apps
+  environment.sessionVariables = {
+    GSETTINGS_SCHEMA_DIR = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
+    NIXOS_OZONE_WL = "1";
+  };
+
   home-manager.users.andrei = {
     home.packages = with pkgs; [
       tela-icon-theme
       noto-fonts
+      gsettings-desktop-schemas
     ];
 
     gtk = {
