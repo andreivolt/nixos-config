e0fbbf8bb9efe40a102972f383894ed20498f04f
Andrei Volt <andrei@avolt.net>
2025-12-01 22:02:11 +0100

    use xdg-desktop-portal-gnome for Settings

--- linux/xdg-portals.nix
+++ linux/xdg-portals.nix
@@ -1,11 +1,28 @@
 {pkgs, ...}: {
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
-    config.common.default = "*";
+    xdgOpenUsePortal = true;
+    # Note: xdg-desktop-portal-hyprland is automatically added by programs.hyprland
     extraPortals = with pkgs; [
-      xdg-desktop-portal-wlr
       xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
     ];
+    # Configure which portal handles which interface
+    config = {
+      common = {
+        default = [ "gtk" ];
+      };
+      hyprland = {
+        default = [ "hyprland" "gtk" ];
+        # Hyprland portal handles these specific interfaces
+        "org.freedesktop.impl.portal.Screenshot" = [ "hyprland" ];
+        "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
+        "org.freedesktop.impl.portal.GlobalShortcuts" = [ "hyprland" ];
+        # GNOME portal for Settings (better dark mode support)
+        "org.freedesktop.impl.portal.Settings" = [ "gnome" ];
+        "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+        "org.freedesktop.impl.portal.AppChooser" = [ "gtk" ];
+      };
+    };
   };
 }
