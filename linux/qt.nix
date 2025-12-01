5408000f9b1fda8a1e130c5611965ae767c6d5fc
Andrei Volt <andrei@avolt.net>
2025-12-01 23:29:48 +0100

    fix Qt dark mode: remove explicit style override

--- linux/qt.nix
+++ linux/qt.nix
@@ -5,11 +5,11 @@
       adwaita-qt6
     ];
 
-    # Use gnome platform theme - Qt apps follow gsettings color-scheme automatically
+    # Use adwaita platform theme - Qt apps follow gsettings color-scheme automatically
+    # Don't set style.name - let the platform theme handle dark/light switching
     qt = {
       enable = true;
       platformTheme.name = "adwaita";
-      style.name = "adwaita";
     };
   };
 }
