49bdc068d4785fe97e87dbaf00a0aa536601d976
Andrei Volt <andrei@avolt.net>
2025-12-01 22:20:13 +0100

    use gnome platform theme for Qt dark/light switching

--- linux/qt.nix
+++ linux/qt.nix
@@ -1,20 +1,15 @@
 {pkgs, ...}: {
   home-manager.users.andrei = {
-    # Provide Qt configuration tools for user customization
     home.packages = with pkgs; [
-      libsForQt5.qt5ct
-      kdePackages.qt6ct
-      # Dark theme packages available for user selection
       adwaita-qt
       adwaita-qt6
-      libsForQt5.qtstyleplugin-kvantum
-      kdePackages.qtstyleplugin-kvantum
     ];
 
-    # Use qt5ct/qt6ct for hybrid approach (Nix provides packages, user configures via GUI)
+    # Use gnome platform theme - Qt apps follow gsettings color-scheme automatically
     qt = {
       enable = true;
-      platformTheme.name = "qtct";
+      platformTheme.name = "gnome";
+      style.name = "adwaita";
     };
   };
 }
