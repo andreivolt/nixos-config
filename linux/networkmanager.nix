fdf7a373d62fca11b176964e4d6991bcd368c218
Andrei Volt <andrei@avolt.net>
2025-12-01 20:46:38 +0100

    make WiFi connections system-wide for pre-login access

--- linux/networkmanager.nix
+++ linux/networkmanager.nix
@@ -11,4 +11,14 @@
   };
 
   users.users.andrei.extraGroups = ["networkmanager"];
+
+  # Make WiFi connections available system-wide (before login)
+  # This removes user-specific permissions from all WiFi connections
+  systemd.services.NetworkManager.preStart = ''
+    for file in /etc/NetworkManager/system-connections/*; do
+      if [ -f "$file" ]; then
+        sed -i '/^permissions=/d' "$file"
+      fi
+    done
+  '';
 }
