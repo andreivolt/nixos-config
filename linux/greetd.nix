6c8b7c217a8794a8a7ec29b93f2644952ba1546f
Andrei Volt <andrei@avolt.net>
2025-12-01 23:54:37 +0100

    fix greetd.tuigreet deprecation warnings

--- linux/greetd.nix
+++ linux/greetd.nix
@@ -1,14 +1,14 @@
 { pkgs, inputs, ... }:
 
 let
-  hyprland = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  hyprland = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
 in {
   services.greetd = {
     enable = true;
     # vt option removed - now fixed to VT1 in nixos-unstable
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --cmd 'uwsm start ${hyprland}/bin/hyprland'";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --cmd 'uwsm start ${hyprland}/bin/hyprland'";
         user = "greeter";
       };
     };
