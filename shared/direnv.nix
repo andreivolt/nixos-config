8b543ba197d3c308c5b3fcbc63390aed7540f0db
Andrei Volt <andrei@avolt.net>
2025-12-05 18:48:54 +0100

    direnv: add whitelist and load_dotenv config

--- shared/direnv.nix
+++ shared/direnv.nix
@@ -1,7 +1,29 @@
 {
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-    silent = true;
-  };
+  lib,
+  pkgs,
+  ...
+}: let
+  isDarwin = pkgs.stdenv.isDarwin;
+  homeDir =
+    if isDarwin
+    then "/Users/andrei"
+    else "/home/andrei";
+in {
+  home-manager.sharedModules = [
+    {
+      programs.direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+        silent = true;
+        config = {
+          global = {
+            load_dotenv = true;
+          };
+          whitelist = {
+            prefix = ["${homeDir}/dev"];
+          };
+        };
+      };
+    }
+  ];
 }
