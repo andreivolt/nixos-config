6c485e51dd2258e0b623d8a8a36cad6fba98a033
Andrei Volt <andrei@avolt.net>
2025-12-05 17:37:06 +0100

    zathura: add home-manager config

---
+++ linux/zathura.nix
@@ -0,0 +1,21 @@
+# Zathura document viewer configuration
+{
+  programs.zathura = {
+    enable = true;
+    options = {
+      font = "Tahoma 11";
+      recolor = true;
+    };
+  };
+
+  # Custom desktop file with proper MimeType for file managers
+  xdg.desktopEntries.zathura = {
+    name = "Zathura";
+    comment = "A minimalistic document viewer";
+    exec = "zathura %U";
+    icon = "org.pwmt.zathura";
+    terminal = false;
+    categories = ["Office" "Viewer"];
+    mimeType = ["application/pdf" "application/epub+zip" "application/oxps" "application/x-fictionbook"];
+  };
+}
