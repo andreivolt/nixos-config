6688cb968555611e8496b6683715e048b2756743
Andrei Volt <andrei@avolt.net>
2025-12-05 02:58:49 +0100

    phone: enable Termux android-integration

--- hosts/phone/default.nix
+++ hosts/phone/default.nix
@@ -21,6 +21,17 @@
     experimental-features = nix-command flakes
   '';
 
+  # Android integration - termux commands
+  android-integration = {
+    termux-open.enable = true;
+    termux-open-url.enable = true;
+    termux-reload-settings.enable = true;
+    termux-setup-storage.enable = true;
+    termux-wake-lock.enable = true;
+    termux-wake-unlock.enable = true;
+    xdg-open.enable = true; # alias to termux-open
+  };
+
   # Home Manager config
   home-manager = {
     useGlobalPkgs = true;
