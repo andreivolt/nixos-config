c9adba3a10165912c06689d7e76cdc8d583566e8
Andrei Volt <andrei@avolt.net>
2025-12-05 19:13:08 +0100

    import parallel, hushlogin, npm, ripgrep modules

--- hosts/mac/default.nix
+++ hosts/mac/default.nix
@@ -17,8 +17,12 @@
     ../../shared/git.nix
     ../../shared/glab.nix
     ../../shared/gnupg.nix
+    ../../shared/hushlogin.nix
     ../../shared/mpv.nix
+    ../../shared/npm.nix
+    ../../shared/parallel.nix
     ../../shared/readline.nix
+    ../../shared/ripgrep.nix
     ../../shared/rubocop.nix
     ../../shared/rustfmt.nix
     ../../shared/tmux.nix
