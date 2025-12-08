56fc880a5c7d65f8a619989711422d20559faf99
Andrei Volt <andrei@avolt.net>
2025-12-08 21:41:05 +0100

    lan-mouse: add declarative config with fingerprints
    Generate config.toml in nix store with peer configuration and
    authorized fingerprints, eliminating need for GUI setup after reboot.
    Cert file persists in ~/.config/lan-mouse/.

--- linux/lan-mouse.nix
+++ linux/lan-mouse.nix
@@ -3,22 +3,42 @@
 let
   hostname = config.networking.hostName;
 
-  # Peer configurations using Tailscale MagicDNS hostnames
+  # Certificate fingerprints for each host (sha256 of lan-mouse.pem)
+  # Must be lowercase - lan-mouse's generate_fingerprint() outputs lowercase hex
+  fingerprints = {
+    riva = "c3:ef:f5:55:e9:d8:05:b5:42:6b:d9:ed:7d:89:f7:b5:8d:6f:a6:db:42:04:04:71:b8:9c:8e:dc:0f:b7:26:f6";
+    watts = "ec:5f:c5:b1:cb:69:0a:18:ba:3a:fd:ac:c2:03:58:e2:4b:24:02:09:54:f6:cf:74:ff:c1:9f:58:56:e8:99:06";
+  };
+
+  # Peer configurations using Tailscale IPs
   peerConfigs = {
     watts = {
-      direction = "right";
+      position = "right";
       peer = "riva";
       ip = "100.64.0.2";
     };
     riva = {
-      direction = "left";
+      position = "left";
       peer = "watts";
       ip = "100.64.0.3";
     };
   };
 
-  hasPeerConfig = peerConfigs ? ${hostname};
   peerConfig = peerConfigs.${hostname} or null;
+
+  configToml = pkgs.writeText "lan-mouse-config.toml" ''
+    port = 4242
+
+    [[clients]]
+    hostname = "${peerConfig.peer}"
+    ips = ["${peerConfig.ip}"]
+    port = 4242
+    position = "${peerConfig.position}"
+    activate_on_startup = true
+
+    [authorized_fingerprints]
+    "${fingerprints.${peerConfig.peer}}" = "${peerConfig.peer}"
+  '';
 in {
   # Open UDP port for lan-mouse
   networking.firewall.allowedUDPPorts = [ 4242 ];
@@ -27,7 +47,6 @@ in {
   hardware.uinput.enable = true;
   users.users.andrei.extraGroups = [ "input" ];
 
-  # Lan Mouse service only - config managed by GUI
   home-manager.users.andrei = { config, pkgs, ... }: {
     # Lan Mouse systemd user service (daemon mode, no GUI)
     systemd.user.services.lan-mouse = {
@@ -38,7 +57,8 @@ in {
       };
       Service = {
         Type = "simple";
-        ExecStart = "${pkgs.lan-mouse}/bin/lan-mouse daemon";
+        # Config from nix store, cert from persistent home
+        ExecStart = "${pkgs.lan-mouse}/bin/lan-mouse --config ${configToml} --cert-path %h/.config/lan-mouse/lan-mouse.pem daemon";
         Restart = "on-failure";
         RestartSec = 3;
       };
