d026f23da3c2a204b69ce38453732d32debfb22a
Andrei Volt <andrei@avolt.net>
2025-12-09 12:25:55 +0100

    waybar: show kbd backlight status based on user toggle, not current brightness

--- linux/waybar/scripts/get-kbd-backlight.sh
+++ linux/waybar/scripts/get-kbd-backlight.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+USER_DISABLED="/tmp/kbd-backlight-user-disabled"
+
 if ! command -v brightnessctl &>/dev/null; then
   echo '{"text": "", "tooltip": "brightnessctl not found"}'
   exit 0
@@ -8,12 +10,15 @@ fi
 if brightnessctl -d kbd_backlight info &>/dev/null; then
   percent=$(brightnessctl -d kbd_backlight -m | cut -d',' -f4 | tr -d '%')
   percent=${percent:-0}
-  if [ "$percent" -eq 0 ]; then
+
+  if [ -f "$USER_DISABLED" ]; then
     class="off"
+    tooltip="Keyboard backlight: disabled"
   else
     class="on"
+    tooltip="Keyboard backlight: enabled ($percent%)"
   fi
-  echo "{\"text\": \"󰌌\", \"tooltip\": \"Keyboard backlight: $percent%\", \"class\": \"$class\"}"
+  echo "{\"text\": \"󰌌\", \"tooltip\": \"$tooltip\", \"class\": \"$class\"}"
 else
   echo '{"text": "", "tooltip": "No keyboard backlight"}'
 fi
