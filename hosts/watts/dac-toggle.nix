8e4fa918ac94e219bef69988f12d7da2e15fb43c
Andrei Volt <andrei@avolt.net>
2025-12-11 15:31:47 +0100

    add dac-toggle script to switch DAC between PipeWire/exclusive
    Toggle script that releases/reclaims the iFi DAC from PipeWire,
    allowing apps like Roon to use it exclusively via ALSA. Shows
    desktop notifications for mode changes.

---
+++ hosts/watts/dac-toggle.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "dac-toggle" ''
+      read -r IFI_ID PROFILE < <(${pkgs.pipewire}/bin/pw-dump | ${pkgs.jq}/bin/jq -r '
+        .[] | select(.info.props["device.name"] // "" | test("iFi"; "i"))
+        | "\(.id) \(.info.params.Profile[0].index)"
+      ')
+
+      if [ -z "$IFI_ID" ]; then
+        ${pkgs.libnotify}/bin/notify-send -u critical "DAC Toggle" "iFi DAC not found"
+        exit 1
+      fi
+
+      if [ "$PROFILE" = "0" ]; then
+        ${pkgs.wireplumber}/bin/wpctl set-profile "$IFI_ID" 1
+        ${pkgs.libnotify}/bin/notify-send -i audio-card "iFi DAC" "PipeWire mode"
+      else
+        ${pkgs.wireplumber}/bin/wpctl set-profile "$IFI_ID" 0
+        ${pkgs.libnotify}/bin/notify-send -i audio-card "iFi DAC" "Exclusive mode (ALSA)"
+      fi
+    '')
+  ];
+}
