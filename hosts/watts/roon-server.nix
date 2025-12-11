54861bd4f359729f08f16a0810f9ff891c2c23ed
Andrei Volt <andrei@avolt.net>
2025-12-11 15:26:18 +0100

    bind roon-server lifecycle to iFi DAC device
    Fixes RAATServer CPU spin bug (100% usage) when the USB audio
    device disconnects or becomes unavailable. Service now auto-stops
    when DAC is unplugged and auto-starts when reconnected.

--- hosts/watts/roon-server.nix
+++ hosts/watts/roon-server.nix
@@ -7,6 +7,13 @@
     openFirewall = true;
   };
 
+  # Only run roon-server when iFi DAC is connected
+  # Fixes CPU spin bug when device disconnects mid-session
+  systemd.services.roon-server = {
+    bindsTo = [ "sys-devices-pci0000:00-0000:00:1d.0-0000:06:00.0-0000:07:02.0-0000:3c:00.0-usb3-3\\x2d1-3\\x2d1.3-3\\x2d1.3:1.0-sound-card0-controlC0.device" ];
+    after = [ "sys-devices-pci0000:00-0000:00:1d.0-0000:06:00.0-0000:07:02.0-0000:3c:00.0-usb3-3\\x2d1-3\\x2d1.3-3\\x2d1.3:1.0-sound-card0-controlC0.device" ];
+  };
+
   # Allow unfree package
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
