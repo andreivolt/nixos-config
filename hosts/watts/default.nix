bf3283923f474f7174c0ecea78825b2d1fa818f8
Andrei Volt <andrei@avolt.net>
2025-12-12 05:23:10 +0100

    watts: add undervolt and tune power limits for i7-7500U
    - Configure throttled with custom extraConfig instead of defaults
    - Add conservative undervolt: -80mV core/cache, -50mV GPU/uncore (AC)
    - Set realistic power limits: PL1=25W, PL2=29W (was 44W, excessive for 15W TDP)
    - Enable HWP_Mode for better boost under load
    - Battery profile: -60mV undervolt, PL1=15W, PL2=20W
    
    Tested stable, ~5-7°C temperature reduction observed.

--- hosts/watts/default.nix
+++ hosts/watts/default.nix
@@ -30,9 +30,54 @@
   networking.hostName = "watts";
   system.stateVersion = "23.11";
 
-  # Fix ThinkPad BD PROCHOT throttling bug
+  # Fix ThinkPad BD PROCHOT throttling bug + undervolt for cooler temps
   # https://wiki.archlinux.org/title/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Throttling_fix
-  services.throttled.enable = true;
+  services.throttled = {
+    enable = true;
+    extraConfig = ''
+      [GENERAL]
+      Enabled: True
+      Sysfs_Power_Path: /sys/class/power_supply/AC*/online
+      Autoreload: True
+
+      [BATTERY]
+      Update_Rate_s: 30
+      PL1_Tdp_W: 15
+      PL1_Duration_s: 28
+      PL2_Tdp_W: 20
+      PL2_Duration_S: 0.002
+      Trip_Temp_C: 85
+      cTDP: 0
+      Disable_BDPROCHOT: False
+
+      [AC]
+      Update_Rate_s: 5
+      PL1_Tdp_W: 25
+      PL1_Duration_s: 28
+      PL2_Tdp_W: 29
+      PL2_Duration_S: 0.002
+      Trip_Temp_C: 95
+      HWP_Mode: True
+      cTDP: 0
+      Disable_BDPROCHOT: False
+
+      [UNDERVOLT.BATTERY]
+      # Conservative undervolt - reduce if unstable
+      CORE: -60
+      GPU: -40
+      CACHE: -60
+      UNCORE: -40
+      ANALOGIO: 0
+
+      [UNDERVOLT.AC]
+      # Conservative undervolt - can try -100/-70 if stable
+      CORE: -80
+      GPU: -50
+      CACHE: -80
+      UNCORE: -50
+      ANALOGIO: 0
+    '';
+  };
 
   # Auto-switch power profiles based on AC/battery
   services.power-profiles-daemon.enable = true;
