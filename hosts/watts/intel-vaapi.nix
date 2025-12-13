cb20f35d6703511c736bdb6c4d487e732c6d6f0f
Andrei Volt <andrei@avolt.net>
2025-12-13 12:52:28 +0100

    configure mpv to use vaapi hardware decoding on watts

--- hosts/watts/intel-vaapi.nix
+++ hosts/watts/intel-vaapi.nix
@@ -1,8 +1,17 @@
-{pkgs, ...}: {
+{pkgs, lib, ...}: {
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
       intel-media-driver
     ];
   };
+
+  # Configure mpv to use VAAPI for hardware decoding
+  home-manager.sharedModules = [
+    {
+      programs.mpv.config = {
+        hwdec = lib.mkForce "vaapi";
+      };
+    }
+  ];
 }
