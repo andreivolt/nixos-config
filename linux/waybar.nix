f7838ba4c0527e3b4e50906cf3fd8c30b46e2609
Andrei Volt <andrei@avolt.net>
2025-12-14 17:27:56 +0100

    use systemd path unit for waybar config watching

--- linux/waybar.nix
+++ linux/waybar.nix
@@ -37,36 +37,27 @@ in
       systemd.enable = true;
     };
 
-    # Auto-restart waybar when CSS changes
-    # moved_to: atomic rename (Claude Edit tool, some editors)
-    # close_write: direct write (vim, manual edits)
-    systemd.user.services.waybar-css-watcher = {
-      Unit = {
-        Description = "Watch waybar CSS for changes";
-        After = [ "waybar.service" ];
-      };
+    # Auto-restart waybar when config files change
+    systemd.user.paths.waybar-config-watcher = {
+      Unit.Description = "Watch waybar config for changes";
+      Path.PathChanged = [
+        "/home/andrei/dev/nixos-config/linux/waybar/style.css"
+        "/home/andrei/dev/nixos-config/linux/waybar/config.json"
+      ];
+      Install.WantedBy = [ "default.target" ];
+    };
+    systemd.user.services.waybar-config-watcher = {
+      Unit.Description = "Reload waybar on config change";
       Service = {
-        Type = "simple";
-        ExecStart = "${pkgs.writeShellScript "waybar-css-watcher" ''
-          ${pkgs.inotify-tools}/bin/inotifywait -m -e moved_to,close_write "$HOME/dev/nixos-config/linux/waybar/" |
-          while read -r dir event file; do
-            if [ "$file" = "style.css" ]; then
-              ${pkgs.systemd}/bin/systemctl --user restart waybar
-            fi
-          done
-        ''}";
-        Restart = "always";
-        RestartSec = 5;
-      };
-      Install = {
-        WantedBy = [ "default.target" ];
+        Type = "oneshot";
+        ExecStart = "${pkgs.procps}/bin/pkill -SIGUSR2 waybar";
       };
     };
 
     # Config files from external sources
     xdg.configFile = {
-      "waybar/config".source = ./waybar/config.json;
-      # Out-of-store symlink for live CSS editing without rebuild
+      # Out-of-store symlinks for live editing without rebuild
+      "waybar/config".source = config.lib.file.mkOutOfStoreSymlink "/home/andrei/dev/nixos-config/linux/waybar/config.json";
       "waybar/style.css" = {
         source = config.lib.file.mkOutOfStoreSymlink "/home/andrei/dev/nixos-config/linux/waybar/style.css";
         onChange = "${pkgs.systemd}/bin/systemctl --user restart waybar || true";
