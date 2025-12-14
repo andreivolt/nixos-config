0df1d8e345b2ba79efc15b49d7ec38d72dbaec22
Andrei Volt <andrei@avolt.net>
2025-12-14 11:19:47 +0100

    add Dolphin config: disable folder colors context menu, set preferences

--- linux/dolphin.nix
+++ linux/dolphin.nix
@@ -18,4 +18,25 @@
     ++ lib.optionals (pkgs.stdenv.hostPlatform.isx86_64) [
       kdePackages.kdegraphics-thumbnailers
     ];
+
+  home-manager.users.andrei = {
+    # Dolphin configuration
+    xdg.configFile."dolphinrc".text = ''
+      [CompactMode]
+      PreviewSize=22
+
+      [KFileDialog Settings]
+      Places Icons Auto-resize=false
+      Places Icons Static Size=22
+
+      [MainWindow]
+      MenuBar=Disabled
+    '';
+
+    # Disable folder color context menu
+    xdg.dataFile."kio/servicemenus/folderscolor.desktop".text = ''
+      [Desktop Entry]
+      Hidden=true
+    '';
+  };
 }
