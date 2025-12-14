e588960d3c6a5eb6e8af3c17227170bb355ebad2
Andrei Volt <andrei@avolt.net>
2025-12-14 10:48:36 +0100

    use hyprland-uwsm.desktop for proper XDG environment setup
    Fixes UWSM notification about XDG_CURRENT_DESKTOP being set incorrectly
    by using the desktop entry instead of the raw binary path.

--- linux/greetd.nix
+++ linux/greetd.nix
@@ -1,14 +1,12 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 
-let
-  hyprland = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-in {
+{
   services.greetd = {
     enable = true;
     # vt option removed - now fixed to VT1 in nixos-unstable
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --cmd 'uwsm start ${hyprland}/bin/hyprland'";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --cmd 'uwsm start hyprland-uwsm.desktop'";
         user = "greeter";
       };
     };
@@ -16,6 +14,6 @@ in {
 
   # Make wayland-sessions available
   environment.etc."greetd/environments".text = ''
-    uwsm start ${hyprland}/bin/hyprland
+    uwsm start hyprland-uwsm.desktop
   '';
 }
