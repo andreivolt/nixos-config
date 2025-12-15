9ce683c506037a2ec617a6957e8e6a3d0350a6ed
Andrei Volt <andrei@avolt.net>
2025-12-15 09:34:52 +0100

    zsh: migrate rc.zsh to native nix options, remove termux
    - use history.*, autocd, setOptions, shellAliases, shellGlobalAliases
    - inline rc.zsh content into initContent
    - add preview writeShellApplication for fzf file preview
    - conditional aliases: open (linux), ssh function (kitty)
    - conditional browser for mpv/yt-dlp: chromium on asahi, chrome elsewhere
    - conditional darwin.zsh sourcing and file placement

--- shared/zsh/darwin.zsh
+++ shared/zsh/darwin.zsh
@@ -1,13 +1,3 @@
-export SHELL_SESSIONS_DISABLE=1
-
-export HOMEBREW_CELLAR=/opt/homebrew/Cellar
-export HOMEBREW_PREFIX=/opt/homebrew
-export HOMEBREW_REPOSITORY=/opt/homebrew
-export INFOPATH=/opt/homebrew/share/info${INFOPATH:+:$INFOPATH}
-export MANPATH=/opt/homebrew/share/man${MANPATH:+:$MANPATH}:
-export LIBRARY_PATH=/opt/homebrew/opt/libiconv/lib${LIBRARY_PATH:+:$LIBRARY_PATH}
-path=(/opt/homebrew/bin /opt/homebrew/sbin $path)
-
 zsh-defer source ~/.orbstack/shell/init.zsh 2>/dev/null
 
 path=(
