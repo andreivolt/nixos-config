7e3f417b6be957377308be70d4cdf2766f55580e
Andrei Volt <andrei@avolt.net>
2025-12-16 11:24:24 +0100

    nix: exclude watts from its own substituters to avoid redundant localhost fetch

--- shared/nix.nix
+++ shared/nix.nix
@@ -19,8 +19,7 @@ in
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
       "https://nixos-apple-silicon.cachix.org"
-      "http://watts:5000"
-    ];
+    ] ++ lib.optionals (!isWatts) [ "http://watts:5000" ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
