c84e5bb13bf52bc83c7b8d53d501f0ca49cff711
Andrei Volt <andrei@avolt.net>
2025-12-16 16:08:15 +0100

    pragmasevka-nerd-font: build from source with custom Light weight
    Replace pre-built binary fetch with Iosevka source build using privateBuildPlan.
    This adds a Light weight (300) that doesn't exist in upstream releases.
    
    Weights: Light (300), Regular (425), SemiBold (600), Bold (800)
    Variants: upright and italic for each weight

--- pkgs/pragmasevka-nerd-font/default.nix
+++ pkgs/pragmasevka-nerd-font/default.nix
@@ -1,24 +1,103 @@
-{ stdenvNoCC, fetchurl, unzip }:
+{ lib
+, stdenvNoCC
+, iosevka
+, nerd-font-patcher
+}:
+
 let
-  pname = "pragmasevka-nerd-font";
-  version = "1.7.0";
-  src = fetchurl {
-    url = "https://github.com/shytikov/pragmasevka/releases/download/v${version}/Pragmasevka_NF.zip";
-    hash = "sha256-7qt1jv9WLRyu12EkRIjlZUW+Jegaa0DNhLMbAyo3YVw=";
+  # Build Pragmasevka from Iosevka source with custom build plan
+  pragmasevka-base = iosevka.override {
+    set = "Pragmasevka";
+    privateBuildPlan = {
+      family = "Pragmasevka";
+      spacing = "term";
+      serifs = "sans";
+      noCvSs = true;
+      exportGlyphNames = true;
+
+      widths.normal = {
+        shape = 500;
+        menu = 5;
+        css = "normal";
+      };
+
+      metricOverride = {
+        leading = 1100;
+        xHeight = 550;
+      };
+
+      ligations = {
+        inherits = "default-calt";
+        enables = [
+          "eqslasheq"
+          "kern-dotty"
+          "kern-bars"
+          "llggeq"
+        ];
+        disables = [ "slash-asterisk" ];
+      };
+
+      variants.inherits = "ss08";
+
+      weights = {
+        light = {
+          shape = 300;
+          menu = 300;
+          css = 300;
+        };
+        regular = {
+          shape = 425;
+          menu = 400;
+          css = 400;
+        };
+        semibold = {
+          shape = 600;
+          menu = 600;
+          css = 600;
+        };
+        bold = {
+          shape = 800;
+          menu = 700;
+          css = 700;
+        };
+      };
+
+      slopes = {
+        upright = "default.Upright";
+        italic = "default.Italic";
+      };
+    };
   };
 in
 stdenvNoCC.mkDerivation {
-  inherit pname version src;
-  dontUnpack = true;
-  nativeBuildInputs = [ unzip ];
-  installPhase = ''
-    unzip ${src}
-    mkdir -p $out/share/fonts/truetype
-    install --mode=644 ./*.ttf $out/share/fonts/truetype
+  pname = "pragmasevka-nerd-font";
+  version = pragmasevka-base.version;
+
+  src = pragmasevka-base;
+
+  nativeBuildInputs = [ nerd-font-patcher ];
+
+  buildPhase = ''
+    runHook preBuild
+    mkdir -p patched
+    for font in $src/share/fonts/truetype/*.ttf; do
+      nerd-font-patcher --complete --careful -out patched "$font"
+    done
+    runHook postBuild
   '';
 
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/share/fonts/truetype
+    install -m644 patched/*.ttf $out/share/fonts/truetype/
+    runHook postInstall
+  '';
+
+  requiredSystemFeatures = [ "big-parallel" ];
+
   meta = {
-    description = "Pragmata Pro doppelgänger made of Iosevka SS08";
+    description = "Pragmata Pro doppelgänger made of Iosevka SS08 with Nerd Font glyphs";
     homepage = "https://github.com/shytikov/pragmasevka";
+    license = lib.licenses.ofl;
   };
 }
