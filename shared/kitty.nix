5406727bea0860992b232c2375c75cf9294aaba9
Andrei Volt <andrei@avolt.net>
2025-12-27 01:59:26 +0100

    add puremd api key to monolith environment

--- shared/kitty.nix
+++ shared/kitty.nix
@@ -147,16 +147,16 @@ in {
           "shift+enter" = "send_text all \\n";
           "cmd+left" = "send_text all \\x1b[1;5D";
           "cmd+right" = "send_text all \\x1b[1;5C";
-          # Font size (AZERTY)
-          "ctrl+plus" = "change_font_size all +0.5";
-          "ctrl+minus" = "change_font_size all -0.5";
-          "ctrl+agrave" = "change_font_size all 0";
+          # Font size
+          "ctrl+plus" = "change_font_size all +0.25";
+          "ctrl+minus" = "change_font_size all -0.25";
+          "ctrl+0" = "change_font_size all 0";
           # Numpad
-          "kitty_mod+kp_add" = "change_font_size all +0.5";
-          "kitty_mod+kp_subtract" = "change_font_size all -0.5";
+          "kitty_mod+kp_add" = "change_font_size all +0.25";
+          "kitty_mod+kp_subtract" = "change_font_size all -0.25";
           # macOS
-          "cmd+plus" = "change_font_size all +0.5";
-          "cmd+minus" = "change_font_size all -0.5";
+          "cmd+plus" = "change_font_size all +0.25";
+          "cmd+minus" = "change_font_size all -0.25";
           "kitty_mod+a>m" = "set_background_opacity +0.1";
           "kitty_mod+a>l" = "set_background_opacity -0.1";
         };
