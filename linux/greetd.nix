96fe6fab506351bf30683190f3e9963be69ca9b8
Andrei Volt <andrei@avolt.net>
2025-12-29 00:05:00 +0100

    greetd: use uwsm for proper systemd session management

--- linux/greetd.nix
+++ linux/greetd.nix
@@ -6,7 +6,7 @@
     # vt option removed - now fixed to VT1 in nixos-unstable
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --cmd 'uwsm start hyprland-uwsm.desktop'";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --cmd 'uwsm start -- hyprland-uwsm.desktop'";
         user = "greeter";
       };
     };
@@ -14,6 +14,6 @@
 
   # Make wayland-sessions available
   environment.etc."greetd/environments".text = ''
-    uwsm start hyprland-uwsm.desktop
+    uwsm start -- hyprland-uwsm.desktop
   '';
 }
