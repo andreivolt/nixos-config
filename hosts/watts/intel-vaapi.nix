76720951222d4fedf11df23e35a0c60d1c9578b1
Andrei Volt <andrei@avolt.net>
2026-01-01 19:01:32 +0100

    intel-vaapi: suppress VDPAU nvidia probe warnings via va_gl driver

--- hosts/watts/intel-vaapi.nix
+++ hosts/watts/intel-vaapi.nix
@@ -6,6 +6,9 @@
     ];
   };
 
+  # Use VA-GL wrapper to avoid VDPAU nvidia probe warnings
+  environment.variables.VDPAU_DRIVER = "va_gl";
+
   # Configure mpv to use VAAPI for hardware decoding
   home-manager.sharedModules = [
     {
