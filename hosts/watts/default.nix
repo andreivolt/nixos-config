6e939e8733a92b5d0d4dc596d5577fe39beca387
Andrei Volt <andrei@avolt.net>
2026-01-03 21:03:05 +0100

    watts: extract throttled config to separate module

--- hosts/watts/default.nix
+++ hosts/watts/default.nix
@@ -32,6 +32,7 @@
     ./ddc.nix
     ./v4l2loopback.nix
     ./distributed-builds.nix
+    ./throttled.nix
   ];
 
   # GLSL screensaver - plasma shader, 10min idle, main monitor only
@@ -42,83 +43,8 @@
     monitor = "eDP-1";  # internal display only
   };
 
-  # On-demand screensaver command
-  environment.systemPackages = [
-    (pkgs.writeShellScriptBin "screensaver" ''
-      # Save current focused monitor and window
-      PREV_MON=$(${pkgs.hyprland}/bin/hyprctl monitors -j | ${pkgs.jq}/bin/jq -r '.[] | select(.focused) | .name')
-      PREV_ADDR=$(${pkgs.hyprland}/bin/hyprctl activewindow -j | ${pkgs.jq}/bin/jq -r '.address')
-
-      # Launch screensaver in background
-      ${pkgs.callPackage ../../pkgs/screensaver {}}/bin/screensaver \
-        --fps 60 --shader plasma --monitor eDP-1 "$@" &
-      PID=$!
-
-      # Kill screensaver on Ctrl+C
-      trap "kill $PID 2>/dev/null" INT TERM
-
-      # Restore focus after brief delay
-      sleep 0.2
-      ${pkgs.hyprland}/bin/hyprctl dispatch focusmonitor "$PREV_MON" >/dev/null
-      [ -n "$PREV_ADDR" ] && [ "$PREV_ADDR" != "null" ] && \
-        ${pkgs.hyprland}/bin/hyprctl dispatch focuswindow "address:$PREV_ADDR" >/dev/null
-
-      # Wait for screensaver to exit
-      wait $PID
-    '')
-  ];
-
   networking.hostName = "watts";
   system.stateVersion = "23.11";
-
-  # Fix ThinkPad BD PROCHOT throttling bug + undervolt for cooler temps
-  # https://wiki.archlinux.org/title/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Throttling_fix
-  services.throttled = {
-    enable = true;
-    extraConfig = ''
-      [GENERAL]
-      Enabled: True
-      Sysfs_Power_Path: /sys/class/power_supply/AC*/online
-      Autoreload: True
-
-      [BATTERY]
-      Update_Rate_s: 30
-      PL1_Tdp_W: 15
-      PL1_Duration_s: 28
-      PL2_Tdp_W: 20
-      PL2_Duration_S: 0.002
-      Trip_Temp_C: 85
-      cTDP: 0
-      Disable_BDPROCHOT: False
-
-      [AC]
-      Update_Rate_s: 5
-      PL1_Tdp_W: 25
-      PL1_Duration_s: 28
-      PL2_Tdp_W: 29
-      PL2_Duration_S: 0.002
-      Trip_Temp_C: 95
-      HWP_Mode: True
-      cTDP: 0
-      Disable_BDPROCHOT: False
-
-      [UNDERVOLT.BATTERY]
-      # Tested stable
-      CORE: -80
-      GPU: -50
-      CACHE: -80
-      UNCORE: -50
-      ANALOGIO: 0
-
-      [UNDERVOLT.AC]
-      # -100mV caused freeze during rebuild, -90mV as compromise
-      CORE: -90
-      GPU: -60
-      CACHE: -90
-      UNCORE: -60
-      ANALOGIO: 0
-    '';
-  };
 
   # Distribute hardware interrupts across CPUs (reduces CPU0 overload, Intel-specific)
   services.irqbalance.enable = true;
