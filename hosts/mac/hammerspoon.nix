c11568ac770682da25b4b8925df6cc61227ee984
Andrei Volt <andrei@avolt.net>
2026-01-03 17:58:28 +0100

    move hammerspoon to darwin/ (macOS-only)

--- hosts/mac/hammerspoon.nix
+++ hosts/mac/hammerspoon.nix
@@ -8,8 +8,8 @@
 
   home-manager.users.andrei.home.file = {
     ".hammerspoon/Spoons/SpoonInstall.spoon/init.lua".source = "${inputs.hammerspoon-spoons}/Source/SpoonInstall.spoon/init.lua";
-    ".hammerspoon/init.lua".source = ../../shared/hammerspoon/init.lua;
-    ".hammerspoon/app-toggle.lua".source = ../../shared/hammerspoon/app-toggle.lua;
-    ".hammerspoon/darkmode.lua".source = ../../shared/hammerspoon/darkmode.lua;
+    ".hammerspoon/init.lua".source = ../../darwin/hammerspoon/init.lua;
+    ".hammerspoon/app-toggle.lua".source = ../../darwin/hammerspoon/app-toggle.lua;
+    ".hammerspoon/darkmode.lua".source = ../../darwin/hammerspoon/darkmode.lua;
   };
 }
