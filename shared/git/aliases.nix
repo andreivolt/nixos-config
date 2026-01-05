340a1300714d4f62ff3bfd43b67266abbb92523a
Andrei Volt <andrei@avolt.net>
2026-01-05 10:46:51 +0100

    git: split log alias into l (current branch) and la (all branches)

--- shared/git/aliases.nix
+++ shared/git/aliases.nix
@@ -11,7 +11,8 @@
   pf = "push -f";
   tree = "!git ls-files | tree --fromfile -a";
   ap = "add --patch";
-  l = "log --exclude='refs/original/*' --all --graph --format=format:'%C(bold blue)%h%C(bold yellow)%d%C(reset) - %C(bold green)%<(14)%ar%C(reset) %s %C(dim white)- %<(12,trunc)%an%C(reset)'";
+  l = "log --exclude='refs/original/*' --graph --format=format:'%C(bold blue)%h%C(bold yellow)%d%C(reset) - %C(bold green)%<(14)%ar%C(reset) %s %C(dim white)- %<(12,trunc)%an%C(reset)'";
+  la = "!git l --all";
   ups = "!git add --update && git commit --amend --reuse-message HEAD && git push --force";
   lb = "!git reflog show --pretty=format:'%gs ~ %gd' --date=relative | grep 'checkout:' | grep -oE '[^ ]+ ~ .*' | awk -F~ '!seen[$1]++' | awk -F' ~ HEAD@{' '{printf(\"  \\033[33m%s: \\033[37m %s\\033[0m\\n\", substr($2, 1, length($2)-1), $1)}'";
   af = "add --force";
