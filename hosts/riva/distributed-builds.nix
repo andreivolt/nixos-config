13b7726770e8482945738386c0b2ee002304ce23
Andrei Volt <andrei@avolt.net>
2026-01-05 10:32:37 +0100

    nix: move LAN cache config from shared to host dirs

--- hosts/riva/distributed-builds.nix
+++ hosts/riva/distributed-builds.nix
@@ -1,4 +1,16 @@
-{
+{ lib, ... }: {
+  # LAN binary caches (watts has higher priority - faster/closer)
+  nix.settings = {
+    substituters = lib.mkAfter [
+      "http://watts:5000?priority=50"
+      "http://ampere:5000"
+    ];
+    trusted-public-keys = [
+      "watts:FSqfFsCS8kQ1S38CJeND7hwRgS778f5sM5yy+rdYnN8="
+      "ampere:VemsKe9KxjJHofpyUnMnGC9jHo6v49nAlKVQf/1rseI="
+    ];
+  };
+
   # Build on watts for x86_64-linux packages
   nix.buildMachines = [{
     hostName = "watts";
