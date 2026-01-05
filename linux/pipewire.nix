3ef9fcb99b30f0478666d3b1f3df90a0e3e3c057
Andrei Volt <andrei@avolt.net>
2026-01-05 20:03:03 +0100

    pipewire: remove low-latency config that caused mpv streaming crackling

--- linux/pipewire.nix
+++ linux/pipewire.nix
@@ -8,14 +8,5 @@
     alsa.support32Bit = true;
 
     pulse.enable = true;
-
-    # Reduce buffer size for lower latency (~20-30ms improvement)
-    extraConfig.pipewire."92-low-latency" = {
-      "context.properties" = {
-        "default.clock.rate" = 48000;
-        "default.clock.quantum" = 256;
-        "default.clock.min-quantum" = 256;
-      };
-    };
   };
 }
