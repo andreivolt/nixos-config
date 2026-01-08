8e1a3fade6e46c01b91a2781261e358dc8072983
Andrei Volt <andrei@avolt.net>
2026-01-08 19:43:42 +0100

    extract Asahi Linux config into linux/asahi/ module

--- linux/asahi/j413-mic.nix
+++ linux/asahi/j413-mic.nix
@@ -1,5 +1,6 @@
-# Set internal mic as default audio source (not headset jack)
-{
+# MacBook Pro 14" (j413) audio configuration
+# Sets internal mic as default source instead of headset jack
+{ ... }: {
   services.pipewire.wireplumber.extraConfig."50-asahi-mic-default" = {
     "wireplumber.settings" = {
       "default.audio.source" = "effect_output.j413-mic";
