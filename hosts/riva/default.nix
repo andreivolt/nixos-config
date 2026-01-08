ad8e7c154da461119fdf6e6caf0b342467c0fc01
Andrei Volt <andrei@avolt.net>
2026-01-08 19:43:42 +0100

    extract Asahi Linux config into linux/asahi/ module

--- hosts/riva/default.nix
+++ hosts/riva/default.nix
@@ -1,7 +1,5 @@
 {
   config,
-  lib,
-  pkgs,
   inputs,
   ...
 }: {
@@ -21,35 +19,15 @@
     # ../../linux/rclone-sync.nix
     ../../linux/zram.nix
     ../../linux/monolith
-    ./battery.nix
     ./distributed-builds.nix
-    ./audio.nix
     ./earlyoom.nix
-    ./shutdown-sound-fix.nix
+    ../../linux/asahi
+    ../../linux/asahi/notch.nix
+    ../../linux/asahi/j413-mic.nix
   ];
 
   networking.hostName = "riva";
   system.stateVersion = "24.05";
-
-  # Apple Silicon support
-  hardware.asahi.setupAsahiSound = true;
-  hardware.asahi.peripheralFirmwareDirectory = ./firmware;
-  hardware.asahi.extractPeripheralFirmware = true;
-
-  # Fix firmware symlink for touchpad (tpmtfw) - kernel looks for apple/ but firmware is in vendor/vendorfw/apple/
-  system.activationScripts.appleFirmwareSymlink = lib.stringAfter ["etc"] ''
-    if [ -d /lib/firmware/vendor/vendorfw/apple ] && [ ! -e /lib/firmware/apple ]; then
-      ln -sf /lib/firmware/vendor/vendorfw/apple /lib/firmware/apple
-    fi
-  '';
-
-  # Boot - Apple Silicon uses m1n1 -> U-Boot -> systemd-boot
-  boot.loader.efi.canTouchEfiVariables = false;
-
-  boot.kernelParams = [
-    "apple_dcp.show_notch=1"       # show bar in notch area like macOS
-    "nvme_apple.flush_interval=0"  # Apple SSDs are slow at flush, this speeds up writes
-  ];
 
   # Lid switch behavior
   services.logind.settings.Login.HandleLidSwitchExternalPower = "lock";
@@ -59,11 +37,6 @@
 
   # don't keep .drv files, rarely needed
   nix.settings.keep-derivations = false;
-
-  # NVMe scheduler - use none, hardware handles queueing
-  services.udev.extraRules = ''
-    ACTION=="add|change", KERNEL=="nvme[0-9]*n[0-9]*", ENV{DEVTYPE}=="disk", ATTR{queue/scheduler}="none"
-  '';
 
   home-manager.users.andrei = import ../../linux/home.nix {
     inherit config inputs;
