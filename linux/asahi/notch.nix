741376337df8d829ad5007d7dabe07b2a48b13da
Andrei Volt <andrei@avolt.net>
2026-01-23 11:48:03 +0100

    fix notch parameter for renamed appledrm kernel module

--- linux/asahi/notch.nix
+++ linux/asahi/notch.nix
@@ -1,5 +1,5 @@
 # Enable notch support for MacBooks with notch displays
 # (MacBook Pro 14", 16", MacBook Air 15")
 { ... }: {
-  boot.kernelParams = [ "apple_dcp.show_notch=1" ];
+  boot.kernelParams = [ "appledrm.show_notch=1" ];
 }
