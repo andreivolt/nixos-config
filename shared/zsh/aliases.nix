92a3c366b238e87d90cc97680ede98c0b1f58491
Andrei Volt <andrei@avolt.net>
2026-01-31 22:08:44 +0100

    move mpv/yt-dlp browser cookie auth to config files with explicit gnomekeyring for Hyprland

--- shared/zsh/aliases.nix
+++ shared/zsh/aliases.nix
@@ -1,9 +1,6 @@
 # Shell aliases
 { lib, pkgs, ... }:
-let
-  isAsahi = pkgs.stdenv.isLinux && pkgs.stdenv.hostPlatform.isAarch64;
-  browser = if isAsahi then "chromium" else "chrome";
-in {
+{
   programs.zsh.shellAliases = {
     "+x" = "chmod +x";
     cdt = "cd $(mktemp -d)";
@@ -20,13 +17,11 @@ in {
     ll = "ls -l --classify=auto --git";
     lla = "ll -a";
     ls = "eza --group-directories-first";
-    mpv = "mpv --ytdl-raw-options=cookies-from-browser=${browser}";
     path = ''printf "%s\n" $path'';
     rg = "rg --smart-case --colors match:bg:yellow --colors match:fg:black";
     rm = "rm --verbose";
     scrcpy = "scrcpy --render-driver opengl";
     vi = "nvim";
-    yt-dlp = "yt-dlp --cookies-from-browser ${browser}";
   } // lib.optionalAttrs pkgs.stdenv.isLinux {
     copy = "wl-copy";
     paste = "wl-paste";
