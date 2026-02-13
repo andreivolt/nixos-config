ea77af6f82a8c5ae61098b6eaccc97803ef5ae8d
Andrei Volt <andrei@avolt.net>
2026-02-13 23:53:16 +0100

    add bypass-paywalls and sci-hub-now as declarative chromium extensions

--- shared/chromium/extensions.nix
+++ shared/chromium/extensions.nix
@@ -3,5 +3,7 @@
   chromium.extensions = [
     inputs.ff2mpv.packages.${pkgs.system}.default
     inputs.dearrow.packages.${pkgs.system}.default
+    inputs.bypass-paywalls.packages.${pkgs.system}.default
+    inputs.sci-hub-now.packages.${pkgs.system}.default
   ];
 }
