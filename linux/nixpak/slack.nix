daf1b04f0f62f768d573efffa06001a6e5b46ea7
Andrei Volt <andrei@avolt.net>
2026-02-20 05:24:06 +0100

    split nixpak.nix into per-app modules under linux/nixpak/

---
+++ linux/nixpak/slack.nix
@@ -0,0 +1,35 @@
+{ pkgs, lib, inputs, ... }:
+let
+  np = import ./lib.nix { inherit pkgs inputs; inherit (pkgs) lib; };
+  inherit (np) mkNixPak guiRoBinds commonRoBinds commonDbusPolices;
+
+  isx86 = pkgs.stdenv.hostPlatform.isx86_64;
+
+  sandboxed = mkNixPak {
+    config = { sloth, ... }: {
+      app.package = pkgs.slack;
+      bubblewrap = {
+        network = true;
+        sockets.wayland = true;
+        sockets.pulse = true;
+        bind.rw = [
+          (sloth.concat' sloth.homeDir "/.config/Slack")
+          (sloth.concat' sloth.homeDir "/Downloads")
+        ];
+        bind.ro = guiRoBinds sloth ++ commonRoBinds;
+        bind.dev = [ "/dev/dri" ];
+      };
+      gpu.enable = true;
+      dbus.enable = true;
+      dbus.policies = commonDbusPolices;
+    };
+  };
+in lib.mkIf isx86 {
+  home-manager.users.andrei.xdg.desktopEntries.slack = {
+    name = "Slack";
+    exec = "${sandboxed.config.env}/bin/slack %U";
+    icon = "slack";
+    terminal = false;
+    categories = ["Network" "InstantMessaging"];
+  };
+}
---
+++ linux/nixpak/telegram.nix
@@ -0,0 +1,34 @@
+{ pkgs, inputs, ... }:
+let
+  np = import ./lib.nix { inherit pkgs inputs; inherit (pkgs) lib; };
+  inherit (np) mkNixPak guiRoBinds commonRoBinds commonDbusPolices;
+
+  sandboxed = mkNixPak {
+    config = { sloth, ... }: {
+      app.package = pkgs.telegram-desktop;
+      bubblewrap = {
+        network = true;
+        sockets.wayland = true;
+        sockets.pulse = true;
+        bind.rw = [
+          (sloth.concat' sloth.homeDir "/.local/share/TelegramDesktop")
+          (sloth.concat' sloth.homeDir "/Downloads")
+        ];
+        bind.ro = guiRoBinds sloth ++ commonRoBinds;
+        bind.dev = [ "/dev/dri" ];
+      };
+      gpu.enable = true;
+      dbus.enable = true;
+      dbus.policies = commonDbusPolices;
+    };
+  };
+in {
+  home-manager.users.andrei.xdg.desktopEntries.telegram = {
+    name = "Telegram";
+    genericName = "Messaging";
+    exec = "${sandboxed.config.env}/bin/Telegram %u";
+    icon = "telegram";
+    terminal = false;
+    categories = ["Network" "InstantMessaging"];
+  };
+}
