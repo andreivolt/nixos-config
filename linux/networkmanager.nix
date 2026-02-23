c896438f092324da09d143529222c50d8b6fb99d
Andrei Volt <andrei@avolt.net>
2026-02-23 01:21:29 +0100

    networkmanager: auto-disable wifi when ethernet is connected

--- linux/networkmanager.nix
+++ linux/networkmanager.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   networking.networkmanager = {
     enable = true;
@@ -11,6 +13,25 @@
   };
 
   users.users.andrei.extraGroups = ["networkmanager"];
+
+  # Disable wifi when ethernet is connected, re-enable when disconnected
+  networking.networkmanager.dispatcherScripts = [{
+    type = "basic";
+    source = pkgs.writeText "wifi-wired-exclusive" ''
+      case "$2" in
+        up)
+          if [ "$(nmcli -g GENERAL.TYPE device show "$1")" = "ethernet" ]; then
+            nmcli radio wifi off
+          fi
+          ;;
+        down)
+          if [ "$(nmcli -g GENERAL.TYPE device show "$1")" = "ethernet" ]; then
+            nmcli radio wifi on
+          fi
+          ;;
+      esac
+    '';
+  }];
 
   # Make WiFi connections available system-wide (before login)
   # This removes user-specific permissions from all WiFi connections
