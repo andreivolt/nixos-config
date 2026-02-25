b1b6b8ad1a6d9822bd9ec351a98f08855d093439
Andrei Volt <andrei@avolt.net>
2026-02-25 16:25:15 +0100

    extract Chrome Web Store extension IDs to shared file for reuse across profiles

--- linux/nixpak/chromium-tor.nix
+++ linux/nixpak/chromium-tor.nix
@@ -9,28 +9,7 @@ let
     "--no-default-browser-check"
   ];
 
-  extensionIds = [
-    "ddkjiahejlhfcafbddmgiahcphecmpfh" # uBlock Origin Lite
-    "edibdbjcniadpccecjdfdjjppcpchdlm" # I still don't care about cookies
-    "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
-    "kehjfphhkfppnnjhdfhanmehkegdppho" # YouTube Row Fixer
-    "fpnmgdkabkmnadcjpehmlllkndpkmiak" # Wayback Machine
-    "nffaoalbilbmmfgbnbgppjihopabppdk" # Video Speed Controller
-    "dhdgffkkebhmkfjojejmpbldmpobfkfo" # Tampermonkey
-    "clngdbkpkpeebahjckkjfobafhncgmne" # Stylus
-    "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock for YouTube
-    "mpiodijhokgodhhofbcjdecpffjipkle" # SingleFile
-    "oahiolknhkbpcolgnpljehalnhblolkm" # ShortsBlocker
-    "gebbhagfogifgggkldgodflihgfeippi" # Return YouTube Dislike
-    "dneaehbmnbhcippjikoajpoabadpodje" # Old Reddit Redirect
-    "kbmfpngjjgdllneeigpgjifpgocmfgmb" # Reddit Enhancement Suite
-    "gneobebnilffgkejpfhlgkmpkipgbcno" # No New Tabs
-    "pbnndmlekkboofhnbonilimejonapojg" # Midnight Lizard
-    "gdnpnkfophbmbpcjdlbiajpkgdndlino" # Infy Scroll
-    "omkfmpieigblcllmkgbflkikinpkodlk" # enhanced-h264ify
-    "iipjdmnoigaobkamfhnojmglcdbnfaaf" # Clutter Free
-    "bbeaicapbccfllodepmimpkgecanonai" # BlockTube
-  ];
+  extensionIds = import ../../shared/chromium/chrome-extensions.nix;
 
   policy = pkgs.writeTextDir "managed/policy.json" (builtins.toJSON {
     JavaScriptBlockedForUrls = [ "[*.]onion" ];
