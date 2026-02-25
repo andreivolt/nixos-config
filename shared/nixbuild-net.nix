7e5d75c22de9cda0d561704a4586c84c56dd0bc0
Andrei Volt <andrei@avolt.net>
2026-02-25 22:18:13 +0100

    derive nixbuild.net substituters from nix.settings to avoid duplication

--- shared/nixbuild-net.nix
+++ shared/nixbuild-net.nix
@@ -1,4 +1,10 @@
-{
+{ config, lib, ... }:
+let
+  allSubstituters = config.nix.settings.substituters;
+  # only public caches are reachable from nixbuild.net
+  substituters = lib.filter (lib.hasPrefix "https://") allSubstituters;
+  keys = lib.unique config.nix.settings.trusted-public-keys;
+in {
   nix.buildMachines = [{
     hostName = "eu.nixbuild.net";
     systems = [ "x86_64-linux" "aarch64-linux" ];
@@ -17,7 +23,7 @@
       ServerAliveInterval 60
       IPQoS throughput
       IdentityFile /home/andrei/.ssh/id_ed25519
-      SetEnv NIXBUILDNET_SUBSTITUTERS="https://cache.nixos.org https://nix-community.cachix.org https://hyprland.cachix.org https://nixos-apple-silicon.cachix.org"
-      SetEnv NIXBUILDNET_TRUSTED_PUBLIC_KEYS="cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc= nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
+      SetEnv NIXBUILDNET_SUBSTITUTERS="${lib.concatStringsSep " " substituters}"
+      SetEnv NIXBUILDNET_TRUSTED_PUBLIC_KEYS="${lib.concatStringsSep " " keys}"
   '';
 }
