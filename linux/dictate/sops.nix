53b52c37a30b13d50b0b46aabcf6ea931075a3b4
Andrei Volt <andrei@avolt.net>
2026-03-04 14:33:37 +0100

    dictate: add Groq and Fireworks API keys to sops template

--- linux/dictate/sops.nix
+++ linux/dictate/sops.nix
@@ -4,6 +4,10 @@ let
 in {
   sops.templates."dictate.env" = {
     owner = "andrei";
-    content = "DEEPGRAM_API_KEY=${p."deepgram/api_key"}";
+    content = ''
+      DEEPGRAM_API_KEY=${p."deepgram/api_key"}
+      GROQ_API_KEY=${p."groq/api_key"}
+      FIREWORKS_API_KEY=${p."fireworks/api_key"}
+    '';
   };
 }
