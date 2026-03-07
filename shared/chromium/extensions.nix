0bbfbf4eca6a2d6ebb5501404a4fe6f82a9f2724
Andrei Volt <andrei@avolt.net>
2026-03-07 18:23:29 +0100

    extract chromium extension list to plain text file

--- shared/chromium/extensions.nix
+++ shared/chromium/extensions.nix
@@ -1,12 +1,8 @@
 { inputs, pkgs, ... }:
-{
-  chromium.extensions = [
-    inputs.ff2mpv.packages.${pkgs.stdenv.hostPlatform.system}.default
-    inputs.dearrow.packages.${pkgs.stdenv.hostPlatform.system}.default
-    inputs.bypass-paywalls.packages.${pkgs.stdenv.hostPlatform.system}.default
-    inputs.sci-hub-now.packages.${pkgs.stdenv.hostPlatform.system}.default
-    inputs.redirect-domains.packages.${pkgs.stdenv.hostPlatform.system}.default
-    inputs.refined-hacker-news.packages.${pkgs.stdenv.hostPlatform.system}.default
-    inputs.userscripts.packages.${pkgs.stdenv.hostPlatform.system}.default
-  ];
+let
+  names = builtins.filter (s: builtins.isString s && s != "")
+    (builtins.split "\n" (builtins.readFile ./extensions.list));
+in {
+  chromium.extensions =
+    map (name: inputs.${name}.packages.${pkgs.stdenv.hostPlatform.system}.default) names;
 }
